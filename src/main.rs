d7f0c7a524b302faa5bab8705a285b76d8563235
Debamitro Chakraborti <debamitro@gmail.com>
2022-11-06 22:06:02 -0500

    New algorithm which counts files as well

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::fs;
-use std::path::Path;
+//use std::path::Path;
+use std::collections::HashMap;
 use std::sync::mpsc::channel;
+use std::sync::mpsc::Sender;
 use std::thread;
 use std::vec::Vec;
 
@@ -19,14 +21,14 @@ impl DirAndSize {
     }
 }
 
-fn findsize(arg1: &Path) -> DirAndSize {
+fn findsize(toplevel: &str, arg1: &str, chan: &Sender<DirAndSize>) {
     let mut size: u64 = 0;
     if let Ok(itr) = fs::read_dir(arg1) {
         for e in itr {
             if let Ok(entry) = e {
                 if let Ok(m) = entry.metadata() {
                     if m.is_dir() {
-                        size += findsize(&entry.path()).size;
+                        findsize(toplevel, entry.path().to_str().unwrap(), chan);
                     } else if m.is_file() {
                         size += m.len();
                     }
@@ -35,12 +37,17 @@ fn findsize(arg1: &Path) -> DirAndSize {
         }
     }
 
-    return DirAndSize::new(arg1.to_str().unwrap(), size / 1024 / 1024);
+    let txi = chan.clone();
+    let arg1_copy = String::from(toplevel);
+    thread::spawn(move || {
+        txi.send(DirAndSize::new(&arg1_copy, size / 1024 / 1024))
+            .unwrap();
+    });
 }
 
 fn findbiggestfiles(dirname: &str, cutoff: u64) {
     if let Ok(itr) = fs::read_dir(dirname) {
-        let mut allsizes: Vec<DirAndSize> = Vec::new();
+        let mut allsizes: HashMap<String, DirAndSize> = HashMap::<String, DirAndSize>::new();
         let mut totalsize: u64 = 0;
 
         let (tx, rx) = channel();
@@ -48,17 +55,20 @@ fn findbiggestfiles(dirname: &str, cutoff: u64) {
             if let Ok(entry) = e {
                 if let Ok(m) = entry.metadata() {
                     if m.is_dir() {
+                        findsize(
+                            entry.path().to_str().unwrap(),
+                            entry.path().to_str().unwrap(),
+                            &tx,
+                        );
+                    } else if m.is_file() {
                         let txi = tx.clone();
 
                         thread::spawn(move || {
-                            txi.send(findsize(&entry.path())).unwrap();
-                        });
-                    }
-                    else if m.is_file() {
-                        let txi = tx.clone();
-
-                        thread::spawn(move || {
-                            txi.send(DirAndSize::new(entry.path().to_str().unwrap(), m.len() / 1024 /1024)).unwrap();
+                            txi.send(DirAndSize::new(
+                                entry.path().to_str().unwrap(),
+                                m.len() / 1024 / 1024,
+                            ))
+                            .unwrap();
                         });
                     }
                 }
@@ -68,17 +78,31 @@ fn findbiggestfiles(dirname: &str, cutoff: u64) {
         let mut lines_printed: u64 = 0;
         while let Ok(msg) = rx.recv() {
             totalsize += msg.size;
-            if msg.size > cutoff {
-                allsizes.push(msg);
+            let entrysize = if let Some(entry) = allsizes.get(&msg.name) {
+                entry.size + msg.size
+            } else {
+                msg.size
+            };
+
+            allsizes.insert(
+                String::from(&msg.name),
+                DirAndSize::new(&msg.name, entrysize),
+            );
+
+            let mut sortedsizes = Vec::new();
+            for v in allsizes.values() {
+                if v.size > cutoff {
+                    sortedsizes.push(v);
+                }
             }
-            allsizes.sort_by(|a, b| b.size.cmp(&a.size));
+            sortedsizes.sort_by(|a, b| b.size.cmp(&a.size));
 
             if lines_printed > 0 {
                 print!("\x1b[{}A\x1b[0J", lines_printed);
             }
             println!("\x1b[31m||File/directory\t|Size in MB||\x1b[0m");
             lines_printed = 1;
-            for a in &allsizes {
+            for a in &sortedsizes {
                 println!("|{}\t|{}|", a.name, a.size);
                 lines_printed += 1;
             }
@@ -90,7 +114,14 @@ fn findbiggestfiles(dirname: &str, cutoff: u64) {
         println!("\x1b[31m||Directory\t|Size in MB||\x1b[0m");
         println!("||Directory\t|Size in MB||");
         println!("|{}\t|{}|", dirname, totalsize);
-        for a in &allsizes {
+        let mut sortedsizes = Vec::new();
+        for v in allsizes.values() {
+            if v.size > cutoff {
+                sortedsizes.push(v);
+            }
+        }
+        sortedsizes.sort_by(|a, b| b.size.cmp(&a.size));
+        for a in &sortedsizes {
             println!("|{}\t|{}|", a.name, a.size);
         }
     }
